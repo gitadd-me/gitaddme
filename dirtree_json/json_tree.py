d78988d5057ce6a628ce316228f2464345d9d5a2
gitadd-me <ikst.yskim@gmail.com>
2022-12-23 22:08:03 +0900

    Update json_tree.py

--- dirtree_json/json_tree.py
+++ dirtree_json/json_tree.py
@@ -12,3 +12,14 @@ def path_to_dict(path):
 def path_to_json(Path):
     import json
     return json.dumps(path_to_dict(Path))
+
+def path_to_txt(path):
+    path = path.replace("\\","/")
+    if path[-1] == "/" :
+        text_file = open(path+'git_dir.json', 'w', encoding = "utf-8")
+        print(path+'Example.txt')
+    else:
+        text_file = open(path+'/git_dir.json', 'w', encoding = "utf-8")
+    json_string = path_to_json(path)
+    text_file.write(json_string)
+    text_file.close()
