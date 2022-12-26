aa213407005cfc9f9568d49ff6ad72d5174d64e9
gitadd-me <ikst.yskim@gmail.com>
2022-12-26 19:23:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='dirtree_json',
-    version='0.0.1',
+    version='0.0.3',
     description='directory path to json',
     url="https://github.com/gitadd-me/gitaddme.git",
     author='KimYongSam',
-    author_email='aaa@gmail.com',
+    author_email='ikst.yskim@gmail.com',
     license='yongsam',
     packages=['dirtree_json'],
     zip_safe=False
